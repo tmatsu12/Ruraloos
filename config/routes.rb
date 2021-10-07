993e9efc9f213050756709a61bce30357488740f
tmatsu <tmatsunaga65@gmail.com>
2021-10-07 08:14:17 +0000

    [Start]modelの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   get 'users/edit'
   get 'users/update'
   root 'homes#top'
-  # devise_scope :user do
-  #   post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
-  # end
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   devise_for :users
   get '/region/:region' => 'homes#region', as: "region"
   resources :posts do
