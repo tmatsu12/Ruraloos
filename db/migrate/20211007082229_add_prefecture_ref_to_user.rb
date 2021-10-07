4f6da9e8ccf008bba8b78628d5902872d6d56d1d
tmatsu <tmatsunaga65@gmail.com>
2021-10-07 08:23:00 +0000

    [Add]modelの作成

---
+++ db/migrate/20211007082229_add_prefecture_ref_to_user.rb
@@ -0,0 +1,6 @@
+class AddPrefectureRefToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :users, :prefecture1, foreign_key: true
+    add_reference :users, :prefecture2, foreign_key: true
+  end
+end
