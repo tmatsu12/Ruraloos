4b09776fb87fa48e200e63ca5a013a1a0221bcb6
tmatsu <tmatsunaga65@gmail.com>
2021-10-07 07:52:23 +0000

    [Finish]ルーティングを記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  root 'homes#top'
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+  devise_for :users
+  get '/region/:region' => 'homes#region', as: "region"
+  resources :posts do
+    resources :post_comments, only: [:new, :create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
+  get 'search' => "searchs#search"
+  get '/about' => 'homes#about'
 end
