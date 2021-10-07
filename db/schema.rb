89fe0735166c4c3a79fc5641ebf48203f39d064e
tmatsu <tmatsunaga65@gmail.com>
2021-10-07 08:27:02 +0000

    [Add]modelの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_07_082416) do
+ActiveRecord::Schema.define(version: 2021_10_07_082621) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,7 +18,9 @@ ActiveRecord::Schema.define(version: 2021_10_07_082416) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "prefecture_id"
+    t.integer "user_id"
     t.index ["prefecture_id"], name: "index_posts_on_prefecture_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "prefectures", force: :cascade do |t|
