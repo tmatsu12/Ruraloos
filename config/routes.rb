3223541d1b050830b9b7b8388eddc186f5f2f498
tmatsu <tmatsunaga65@gmail.com>
2021-10-07 08:02:16 +0000

    [Start]deviseのセットアップ

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   get 'users/edit'
   get 'users/update'
   root 'homes#top'
-  devise_scope :user do
-    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
-  end
-  devise_for :users
+  # devise_scope :user do
+  #   post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  # end
+  # devise_for :users
   get '/region/:region' => 'homes#region', as: "region"
   resources :posts do
     resources :post_comments, only: [:new, :create, :destroy]
