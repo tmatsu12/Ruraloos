9d198eb12c7d44610d46d533b910523a93a8eae4
tmatsu <tmatsunaga65@gmail.com>
2021-10-07 22:45:35 +0000

    [Finish]投稿モデルファイルの作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  belongs_to :prefecture
+  belongs_to :user
+  has_many :post_comments, dependent: :destroy
+  attachment :image
+  geocoded_by :address
+
+  validates :title, presence: true
+  validates :city, presence: true
+  # validates :evaluation, presence: true
+  validates :title, length: { maximum: 25 }
+  validates :city, length: { maximum: 20 }
+  validates :body, length: { maximum: 1000 }
+  validates :body1, length: { maximum: 1000 }
+  validates :body2, length: { maximum: 1000 }
+  validates :body3, length: { maximum: 1000 }
 end
