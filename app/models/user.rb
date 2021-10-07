cfaa83f213c012d9c5c2a5861eb530d214c043af
tmatsu <tmatsunaga65@gmail.com>
2021-10-07 22:43:48 +0000

    [Finish]ユーザーモデルファイルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト"
+    end
+  end
+
+  belongs_to :prefecture1, class_name: "Prefecture", optional: true
+  belongs_to :prefecture2, class_name: "Prefecture", optional: true
+  has_many :posts, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+
+  attachment :profile_image
+
+  validates :name, presence: true
+  # validates :prefecture1_id, presence: true
+  # validates :prefecture2_id, presence: true
+
+  validates :name, length: { maximum: 20 }
 end
