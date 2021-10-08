4501ef9ed127b294139f2c8cd350f045347d43c9
tmatsu <tmatsunaga65@gmail.com>
2021-10-08 23:01:49 +0000

    [Add]ユーザー編集ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,14 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+      flash[:notice] = "ユーザー情報を更新しました"
+    else
+      render :edit
+    end
+  end
 end
