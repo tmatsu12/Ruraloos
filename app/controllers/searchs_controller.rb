eeac16d61b645cb84a3db062643198c7228a5564
tmatsu <tmatsunaga65@gmail.com>
2021-10-08 01:40:17 +0000

    [Add]サイドバーに検索機能の実装

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,2 +1,21 @@
 class SearchsController < ApplicationController
+  def search
+    # @model = params[:model]
+    @user = current_user
+    @content = params[:content]
+    @method = params[:method]
+    @records = search_for(@content, @method)
+  end
+
+  private
+
+  def search_for(content, method)
+    # if model == 'post'
+    if method == 'perfect'
+      Post.where(city: content)
+    else
+      Post.where('city LIKE ?', "%#{content}%")
+    end
+    # end
+  end
 end
