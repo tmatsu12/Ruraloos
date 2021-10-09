77abb089fb6131b30c031ea74c48d7848f8de316
tmatsu <tmatsunaga65@gmail.com>
2021-10-09 02:37:12 +0000

    [Add]RSpecによるテストの実装

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -19,3 +19,23 @@ describe 'エラー①：ユーザー新規登録のテスト' do
     end
   end
 end
+
+describe 'エラー②：フレンドリーフォワーディングの確認（新規登録・ログイン後の画面遷移先のテスト）' do
+  let!(:prefecture) { create(:prefecture) } # !がないとindex pageのidとnew pageのURLのidが一致しない？(prefecture.id = within(35..38)とした時) → そんなことはなかった
+
+  before do
+    visit '/posts?prefecture_id=' + prefecture.id.to_s
+    click_link "新規に投稿する"
+  end
+
+  context '新規投稿を押下後に新規登録した場合' do
+    let(:user) { build(:user) }
+
+    before do
+      click_link '新規登録'
+      fill_in 'user[name]', with: user.name
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: user.password
+      fill_in 'user[password_confirmation]', with: user.password_confirmation
+      click_button "新規登録"
+    end
