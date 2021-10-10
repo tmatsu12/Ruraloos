26836f1255f823b65ce5365fc999d33936e04edd
tmatsu <tmatsunaga65@gmail.com>
2021-10-10 09:00:20 +0000

    [Add]通知表示の作成

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,14 +1,14 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.passive_notifications.page(params[:page]).per(1)
+    @notifications = current_user.passive_notifications.page(params[:page]).per(2)
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
   end
 
   def destroy_all
-      #通知を全削除
-        @notifications = current_user.passive_notifications.destroy_all
-        redirect_to root_path
+    #通知を全削除
+    @notifications = current_user.passive_notifications.destroy_all
+    redirect_to root_path
   end
 end
