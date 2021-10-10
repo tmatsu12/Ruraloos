dc3f5ef47c9394c5424328992af7b033abef0294
tmatsu <tmatsunaga65@gmail.com>
2021-10-10 02:44:39 +0000

    [Add]通知の表示の作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -35,7 +35,7 @@ class Post < ApplicationRecord
       action: 'comment'
     )
     # 自分の投稿に対するコメントの場合は、通知済みとする
-    if notification.visitor_id == notification.visited_id
+    if notification.visiter_id == notification.visited_id
       notification.checked = true
     end
     notification.save if notification.valid?
