5c3da1e182a3c07a62addcd703749d61d210692b
tmatsu <tmatsunaga65@gmail.com>
2021-10-10 23:28:16 +0000

    [Finish]通知表示の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image, :prefecture1_id, :prefecture2_id)
+    params.require(:user).permit(:name, :profile_image, :prefecture1_id, :prefecture2_id, :introduction)
   end
 
   def ensure_correct_user
