a8d161f9151765f2045f6f0d130e1b9675e3f3d3
tmatsu <tmatsunaga65@gmail.com>
2021-10-10 01:25:23 +0000

    [Add]コントローラの作成

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,14 @@
 class NotificationsController < ApplicationController
+  def index
+    @notifications = current_user.passive_notifications.page(params[:page]).per(3)
+    @notifications.where(checked: false).each do |notification|
+      notification.update_attributes(checked: true)
+    end
+  end
+
+  def destroy_all
+      #通知を全削除
+        @notifications = current_user.passive_notifications.destroy_all
+        redirect_to root_path
+  end
 end
