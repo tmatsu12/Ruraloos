f3dc478ea2eafd93c3d37c8cbddb6bc675034cdc
tmatsu <tmatsunaga65@gmail.com>
2021-10-10 10:55:05 +0000

    [Add]通知表示の作成

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -3,6 +3,7 @@ class PostComment < ApplicationRecord
   belongs_to :post
   belongs_to :parent, class_name: 'PostComment', optional: true
   has_many   :replies, class_name: 'PostComment', foreign_key: :parent_id, dependent: :destroy
+  has_many :notifications, dependent: :destroy
 
   validates :comment, presence: true
   validates :comment, length: { maximum: 1000 }
