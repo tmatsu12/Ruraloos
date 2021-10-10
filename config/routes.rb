26820abccb2d8369f430e24328ff081178bd05e2
tmatsu <tmatsunaga65@gmail.com>
2021-10-10 00:15:04 +0000

    [Add]ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   get 'search' => "searchs#search"
   get '/about' => 'homes#about'
+  resources :notifications, only: :index
 end
