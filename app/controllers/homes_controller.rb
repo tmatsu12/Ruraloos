f258e7676426b0215a9801f698807b1dff46a1d4
tmatsu <tmatsunaga65@gmail.com>
2021-10-30 11:38:33 +0000

    [Fix]表示に関する微修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def top
-    @posts = Post.all.reverse_order.page(params[:page]).per(4)
+    @posts = Post.all.page(params[:page]).order(updated_at: :desc).per(4)
   end
 
   def about
