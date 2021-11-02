7044c3a511c29e49a439a43270e6aa3a9832e7f0
tmatsu <tmatsunaga65@gmail.com>
2021-11-02 01:51:32 +0000

    [Fix]表示に関する微修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'homes#top'
+  get '/about' => 'homes#about'
+  devise_for :users
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
-  devise_for :users
   get '/region/:region' => 'homes#region', as: "region"
   resources :posts do
     collection do
@@ -13,6 +14,5 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:show, :edit, :update]
   get 'search' => "searchs#search"
-  get '/about' => 'homes#about'
   resources :notifications, only: [:index, :update]
 end
