6d59581c6c707a081149977ea8b43761db088729
tmatsu <tmatsunaga65@gmail.com>
2021-11-06 08:43:43 +0000

    [Update]ユーザーと県の中間テーブルを作成

--- app/controllers/user_prefectures_controller.rb
+++ app/controllers/user_prefectures_controller.rb
@@ -6,12 +6,14 @@ class UserPrefecturesController < ApplicationController
   end
 
   def create
-    @user_prefecture = UserPrefecture.new(user_prefecture_params)
-    if @user_prefecture.save
-      redirect_to user_path(current_user)
-    else
-      render :new
+    @user = current_user
+    params[:user_prefecture][:prefecture_livepast_ids].each do |pref_id|
+      @user.user_prefectures.create!(status: :livepast, prefecture_id: pref_id ) if pref_id.present?
     end
+    params[:user_prefecture][:prefecture_livefuture_ids].each do |pref_id|
+      @user.user_prefectures.create!(status: :livefuture, prefecture_id: pref_id ) if pref_id.present?
+    end
+    redirect_to user_path(@user)
   end
 
   def edit
@@ -21,17 +23,26 @@ class UserPrefecturesController < ApplicationController
 
   def update
     @user = current_user
-    @user_prefecture = UserPrefecture.find_by(user_id: @user.id)
-    if @user_prefecture.update(user_prefecture_params)
+    @user.user_prefectures.delete_all
+    #@user_prefecture = UserPrefecture.find_by(user_id: @user.id)
+  #  if @user_prefecture.update(user_prefecture_params)
+      params[:user_prefecture][:prefecture_livepast_ids].each do |pref_id|
+        @user.user_prefectures.create!(status: :livepast, prefecture_id: pref_id ) if pref_id.present?
+      end
+
+      params[:user_prefecture][:prefecture_livefuture_ids].each do |pref_id|
+        @user.user_prefectures.create!(status: :livefuture, prefecture_id: pref_id ) if pref_id.present?
+      end
+    #if @user.user_prefectures.create!(status: :livepast, prefecture_id: params[:user_prefecture][:prefecture_livepast_ids] )
       redirect_to user_path(@user)
-    else
-      render :edit
-    end
+    #else
+    #  render :edit
+    #end
   end
 
   private
 
   def user_prefecture_params
-    params.require(:user_prefecture).permit(:user_id, :prefecture_id, :status)
+    params.require(:user_prefecture).permit(:user_id, :prefecture_livepast_ids, :prefecture_livefuture_ids)
   end
 end
