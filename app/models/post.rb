9105db45aae0766e49489253987d35b259422ecf
tmatsu <tmatsunaga65@gmail.com>
2021-11-07 06:55:27 +0000

    [Finish]トラッキング機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ApplicationRecord
   has_many :notifications, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  is_impressionable
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
