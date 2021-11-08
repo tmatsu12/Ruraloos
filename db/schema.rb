3e87ea083391a8fa21df8a238925831498f11acf
tmatsu <tmatsunaga65@gmail.com>
2021-11-08 11:46:53 +0000

    [Fix]表示に関する微修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_07_060729) do
+ActiveRecord::Schema.define(version: 2021_11_08_110601) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 2021_11_07_060729) do
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
     t.text "introduction"
+    t.boolean "is_deleted", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
