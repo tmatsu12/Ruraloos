cae8f3ec80509642bb8edbdae5191c4a105c6d30
tmatsu <tmatsunaga65@gmail.com>
2021-11-08 12:01:45 +0000

    [Fix]表示に関する微修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_08_110601) do
+ActiveRecord::Schema.define(version: 2021_11_08_114753) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
-    t.integer "post_comment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -108,7 +107,6 @@ ActiveRecord::Schema.define(version: 2021_11_08_110601) do
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
     t.text "introduction"
-    t.boolean "is_deleted", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
