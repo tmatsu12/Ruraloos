9d3ede2552a610498fcd2823d57eebbee651a8b5
tmatsu <tmatsunaga65@gmail.com>
2021-11-11 12:25:59 +0000

    [Update]リファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,5 @@ Rails.application.routes.draw do
   get 'inquiry'         => 'inquiry#index'     # 入力画面
   post 'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
   post 'inquiry/thanks'  => 'inquiry#thanks'    # 送信完了画面
-  get 'map' => "maps#map"
 end
 
