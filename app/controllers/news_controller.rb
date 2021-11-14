08c6b782feb46856dbf3997c2020c3744cc545f7
tmatsu <tmatsunaga65@gmail.com>
2021-11-14 08:32:13 +0000

    [Add]ヒートマップの追加

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -3,5 +3,7 @@ class NewsController < ApplicationController
     require 'news-api'
     news = News.new(ENV['NEWSAPI'])
     @news = news.get_everything(q: '%E5%9C%B0%E6%96%B9%E7%A7%BB%E4%BD%8F', qInTitle: '%E5%9C%B0%E6%96%B9%E7%A7%BB%E4%BD%8F', sortBy: 'relevancy', pageSize: '5')
+    require 'csv'
+    @prefecture_data = CSV.read("PrefectureData.csv")
   end
 end
