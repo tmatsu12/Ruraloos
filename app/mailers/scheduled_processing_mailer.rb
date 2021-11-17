762f71daadf9dbca94734ffde8dfd0efb226195e
tmatsu <tmatsunaga65@gmail.com>
2021-11-17 14:14:08 +0000

    [Add]バッチ処理の導入

--- app/mailers/scheduled_processing_mailer.rb
+++ app/mailers/scheduled_processing_mailer.rb
@@ -5,6 +5,6 @@ class ScheduledProcessingMailer < ApplicationMailer
       user.passive_notifications.where(visited_id: user.id, checked: false).count >= 10
     end
     users_with_unckecked_notices_mails = users_with_unckecked_notices.pluck(:email)
-    mail(subject: "未読の通知が10件以上あります", bcc: users_with_unckecked_notices_mails)
+    mail(from: '"Ruraloos" <noreply@yoursite.com>', subject: "未読の通知が10件以上あります", bcc: users_with_unckecked_notices_mails)
   end
 end
