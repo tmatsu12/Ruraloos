c418199950bfff6a88541de1e007676aa643c7eb
tmatsu <tmatsunaga65@gmail.com>
2021-11-19 12:52:51 +0000

    [Add]管理者画面の作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create(email: ENV['MAIL_ADDRESS'], password: "111111")
+Admin.create(email: "admin@gmail.com", password: "111111")
