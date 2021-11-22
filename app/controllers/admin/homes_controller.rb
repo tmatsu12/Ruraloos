d09b6577ec2d5658a9c8f6edb95c6c6adee7b4cb
tmatsu <tmatsunaga65@gmail.com>
2021-11-22 02:58:42 +0000

    [Add]管理者機能に追加

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,7 +2,9 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
 
   def top
-    @res1_sql = ActiveRecord::Base.connection.select_all('select name from users;')
-    @res1_AR = User.all.pluck(:name)
+    @res1_sql = ActiveRecord::Base.connection.select_all('Select name From users;')
+    @res1_AR = User.pluck(:name)
+    @res2_sql = ActiveRecord::Base.connection.select_all('SELECT users.name, COUNT(*) FROM posts INNER JOIN users ON users.id = posts.user_id GROUP BY users.name;')
+    @res2_AR = Post.joins(:user).group(:name).count
   end
 end
