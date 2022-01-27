ddcb12bf8af8f5ca794ee10fd168ba6f5ff6cd7e
tmatsu <tmatsunaga65@gmail.com>
2022-01-27 11:46:54 +0000

    [Add]topとregionページにコメントを追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
   def top
     @posts = Post.all.includes(:prefecture, :user).page(params[:page]).order(updated_at: :desc).per(4)
+    # ランキング用のハッシュ。県名がキーで、数が値
     @prefs_livefuture = Prefecture.joins(:user_prefectures).where("user_prefectures.status": "livefuture").group("prefectures.name").order("count_all desc").limit(3).count
   end
 
@@ -8,6 +9,8 @@ class HomesController < ApplicationController
   end
 
   def region
+    # マップの地方をクリックすると、ルーティングに従い、params[:region]に地方名が入る
+    # ルーティングによるとhomes#regionで処理される。views/region/地方名が表示される
     render "region/#{params[:region]}"
   end
 end
