1920a8f7c7d6d6f688ed703adb573280fdeacfd3
tmatsu <tmatsunaga65@gmail.com>
2022-02-08 10:19:14 +0000

    [Add]ActiveRecordに関するコメントを追加

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,6 +4,7 @@ module PostsHelper
       Post.all.includes(:prefecture, :user).order(updated_at: :desc)
     else
       # findの引数が配列なので、sort_postsの戻り値も配列になる
+      # Post.joins(:favorites).group("posts.id").order("count_all desc").countでも良い
       Post.includes(:prefecture, :user).find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))
     end
   end
